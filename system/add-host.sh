f057de85c8523a675d7bac5b290728efa72b602d
kayroy <30625359+kayroy@users.noreply.github.com>
2023-10-20 23:19:04 +0800

    Add files via upload

--- system/add-host.sh
+++ system/add-host.sh
@@ -11,29 +11,7 @@ GitUser="kayroy"
 # // MY IPVPS
 export MYIP=$(curl -sS ipv4.icanhazip.com)
 
-# // VALID SCRIPT
-clear
-VALIDITY () {
-    today=`date -d "0 days" +"%Y-%m-%d"`
-    Exp1=$(curl -sS https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | grep $MYIP | awk '{print $4}')
-    if [[ $today < $Exp1 ]]; then
-    echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
-    else
-    echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m";
-    echo -e "\e[31mPlease renew your ipvps first\e[0m"
-    exit 0
-fi
-}
-IZIN=$(curl -sS https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
-if [ $MYIP = $IZIN ]; then
-echo -e "\e[32mPermission Accepted...\e[0m"
-VALIDITY
-sleep 0.1
-else
-echo -e "\e[31mPermission Denied!\e[0m";
-echo -e "\e[31mPlease buy script first\e[0m"
-exit 0
-fi
+
 
 # // GMAIL && DOMAIN
 clear
--- system/backup.sh
+++ system/backup.sh
@@ -50,7 +50,7 @@ id=(`echo $url | grep '^https' | cut -d'=' -f2`)
 link="https://drive.google.com/u/4/uc?id=${id}&export=download"
 clear
 echo -e "\033[1;37mVPS Data Backup By kayroy\033[0m
-\033[1;37mTelegram : https://t.me/todfix667 / @kayroy\033[0m"
+\033[1;37mTelegram : lembu.com / @kayroy\033[0m"
 echo ""
 echo "Please Copy Link Below & Save In Notepad"
 echo ""
